ec6760dfcf58da348ea3cb7ec07efc43e5651efe
afsumeem <afsumeem@gmail.com>
2022-01-27 23:10:55 +0600

    done

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ async function run() {
             const date = req.body.date;
             const time = req.body.time;
             const status = req.body.status;
+            const pic2 = req.body.pic;
             const rating = req.body.rating;
             const pic = req.files.image;
 
@@ -56,7 +57,7 @@ async function run() {
                 rating,
                 status,
                 comment,
-                pic,
+                pic2,
                 image: imgBuffer
             };
             const result = await blogCollection.insertOne(brand);
